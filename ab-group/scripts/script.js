ec0e317b8b9bd7636c10a6d271d4c8e9532051a5
dadBenders <ntnefimov@gmail.com>
2024-03-18 19:44:58 +0300

    pzdc

--- ab-group/scripts/script.js
+++ ab-group/scripts/script.js
@@ -1,27 +1,26 @@
- 
-const swiper = new Swiper(".card__swiper", {
-  // navigation: {
-  //   nextEl: '.reviews__button_next',
-  //   prevEl: '.reviews__button_prev'
-  // },
-  // loop: true,
-  centeredSlides: true,
-  spaceBetween: 20,
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-    },
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    slidesPerGroup: 1,
-    breakpoints: {
-      1506: {
-        // slidesPerView: 1,
-      }
-    }
-  }
-)
+// const swiper = new Swiper(".card__swiper", {
+//   // navigation: {
+//   //   nextEl: '.reviews__button_next',
+//   //   prevEl: '.reviews__button_prev'
+//   // },
+//   // loop: true,
+
+//   breakpoints: {
+//     690: {
+//       centeredSlides: true,
+//       spaceBetween: 20,
+//       slidesPerView: "auto",
+//       pagination: {
+//         el: ".swiper-pagination",
+//         type: "bullets",
+//       },
+//       loop: true,
+//       navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//       },
+//       slidesPerGroup: 1,
+//       // slidesPerView: 1,
+//     },
+//   },
+// });
