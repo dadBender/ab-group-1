0d26a23769b9f5d874fa9f80867c6b1804c66779
dadBenders <ntnefimov@gmail.com>
2024-03-18 16:30:39 +0300

    asas

--- ab-group/scripts/script.js
+++ ab-group/scripts/script.js
@@ -1,19 +1,27 @@
-
-
+ 
 const swiper = new Swiper(".card__swiper", {
-  navigation: {
-    nextEl: '.reviews__button_next',
-    prevEl: '.reviews__button_prev'
-  },
-  loop: true,
+  // navigation: {
+  //   nextEl: '.reviews__button_next',
+  //   prevEl: '.reviews__button_prev'
+  // },
+  // loop: true,
   centeredSlides: true,
-  slidesPerView: 'auto',
-  spaceBetween: 0,
-  speed: 1500,
-  touchRatio: 1,
-  breakpoints: {
-    951: {
-      spaceBetween: 20,
+  spaceBetween: 20,
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerGroup: 1,
+    breakpoints: {
+      1506: {
+        // slidesPerView: 1,
+      }
     }
   }
-})
+)
